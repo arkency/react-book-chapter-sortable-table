4c3a0f0e766340ae47da30617aafc71057b45610
Fernando <krajka.bartosz@gmail.com>
2015-07-28 14:14:14 +0200

    Setting up the environment. For now we have a single JS file in app/components dir, and it contains only a dummy component rendering nothing.

---
+++ app/components/SortableTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SortableTable = React.createClass({
+  render () {
+    return null;
+  }
+});
+
+export default SortableTable;
--- app/index.js
+++ app/index.js
@@ -1,26 +1,9 @@
-import 'normalize.css'
-import './styles.css'
+import './styles.css';
 
-import debug from 'debug'
-import React from 'react'
-import Hello from './components/Hello'
+import React from 'react';
+import SortableTable from './components/SortableTable';
 
-const log = debug('application:bootstrap')
+const APP_ROOT = document.createElement('div');
+document.body.appendChild(APP_ROOT);
 
-// Enable debug messages outside of production
-if (process.env.NODE_ENV !== 'production') {
-  debug.enable('application:*')
-}
-
-log('creating application node')
-const applicationNode = document.createElement('div')
-applicationNode.className = 'application'
-applicationNode.id = 'application'
-
-log('adding application node to body')
-document.body.appendChild(applicationNode)
-
-log('mounting application')
-React.render(<Hello/>, applicationNode, () => {
-  log('finished mounting application')
-})
+React.render(<SortableTable />, APP_ROOT);
