3bfb19118948c42462060768cdad7f699c35171e
Paweł Pacana <pawel.pacana@gmail.com>
2017-07-21 22:42:22 +0200

    Upgrade React dependency.

--- app/index.js
+++ app/index.js
@@ -2,9 +2,10 @@ import './styles.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import SortableTable from './components/SortableTable';
 
 const APP_ROOT = document.createElement('div');
 document.body.appendChild(APP_ROOT);
 
-React.render(<SortableTable />, APP_ROOT);
+ReactDOM.render(<SortableTable />, APP_ROOT);
