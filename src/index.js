81383d0d1efaeb0dcd53d601347881fd913956b8
Paweł Pacana <pawel.pacana@gmail.com>
2017-08-16 19:53:09 +0200

    Runtime on create-react-app.
    https://github.com/facebookincubator/create-react-app

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import SortableTable from "./components/SortableTable";
+
+ReactDOM.render(<SortableTable />, document.getElementById("root"));
